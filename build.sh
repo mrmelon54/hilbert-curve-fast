8cc003fbd34732282501723f5eb801869f0e79be
MrMelon54 <github@mrmelon54.com>
2025-04-25 21:07:23 +0100

    Build binaries into dist/

--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
-CGO_ENABLED=0 go build -a -ldflags="-w -s" -trimpath -o ./hilbert-curve-fast.x86_64 .
-CGO_ENABLED=0 GOOS=windows go build -a -ldflags="-w -s" -trimpath -o ./hilbert-curve-fast.exe .
+mkdir -p dist/
+
+CGO_ENABLED=0 go build -a -ldflags="-w -s" -trimpath -o ./dist/hilbert-curve-fast.x86_64 .
+CGO_ENABLED=0 GOOS=windows go build -a -ldflags="-w -s" -trimpath -o ./dist/hilbert-curve-fast.exe .
